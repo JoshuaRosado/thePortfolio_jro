cffb33a3f4f0b966b55929d0140642990a18104d
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2024-01-05 16:03:34 -0600

    language selection fixed in laptop mode

--- script.js
+++ script.js
@@ -182,10 +182,10 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+            entry.target.classList.add('show');}
+        // } else {
+        //     entry.target.classList.remove('show');
+        // }
         
     });
 })
